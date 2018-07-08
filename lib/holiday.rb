b37ac359e507c02ed6613885fc7a7586084c29f9
chejianchao <820992376@qq.com>
2018-07-08 19:38:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each {|holiday, hash|
     puts "#{holiday.to_s.capitalize!}:"
     hash.each{|name, val|
+      name.split("_")
       puts "  #{name.to_s.capitalize!}: #{val.join(', ')}"
     }
   }
